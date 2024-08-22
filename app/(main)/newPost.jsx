f98b240b64a92970de9b48db55d1ee0174b7e27a
paralexdisha7 <paraledisha20@gmail.com>
2024-08-23 00:31:53 +0530

    create post service complete

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,4 +1,13 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useRef, useState } from "react";
 import ScreenWrapper from "../../components/screenWrapper";
 import Header from "../../components/Header";
@@ -8,6 +17,12 @@ import Avatar from "../../components/Avatar";
 import { useAuth } from "../../contexts/AuthContext";
 import RichTextEditor from "../../components/RichTextEditor";
 import { useRouter } from "expo-router";
+import Icon from "../../assets/icons";
+import Button from "../../components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { getSupabaseFileUrl } from "../../services/imageService";
+import { Video } from "expo-av";
+import { createOrUpdatePost } from "../../services/postService";
 
 const NewPost = () => {
   const { user } = useAuth();
@@ -16,6 +31,77 @@ const NewPost = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(false);
+  const onPick = async (isImage) => {
+    let mediaConfig = {
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 5],
+      quality: 0.7,
+    };
+    if (!isImage) {
+      mediaConfig = {
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing: true,
+      };
+    }
+    let result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+    // console.log("file", result.assets[0]);
+    if (!result.canceled) {
+      setFile(result.assets[0]);
+    }
+  };
+
+  const isLocalFile = (file) => {
+    if (!file) return null;
+    if (typeof file == "object") return true;
+    return false;
+  };
+  const getFileType = (file) => {
+    if (!file) return null;
+    if (isLocalFile(file)) {
+      return file.type;
+    }
+
+    // check for remote file
+    if (file.includes("postImages")) {
+      return "image";
+    }
+    return "video";
+  };
+
+  const getFileUri = (file) => {
+    if (!file) return null;
+    if (isLocalFile(file)) {
+      return file.uri;
+    }
+    return getSupabaseFileUrl(file)?.uri;
+  };
+  const onSubmit = async () => {
+    if (!bodyRef.current && !file) {
+      Alert.alert("Post", "Please choose an image or post body");
+      return;
+    }
+    let data = {
+      file,
+      body: bodyRef.current,
+      userid: user?.id,
+    };
+
+    // create post
+    setLoading(true);
+    let res = await createOrUpdatePost(data);
+    setLoading(false);
+    console.log("res Post : ", res);
+    if (res.success) {
+      setFile(null);
+      bodyRef.current = "";
+      editorRef.current?.setContentHTML("");
+      router.back();
+    } else {
+      Alert.alert("Post", res.msg);
+    }
+  };
+
   return (
     <ScreenWrapper bg="white">
       <View style={styles.container}>
@@ -34,7 +120,47 @@ const NewPost = () => {
               onChange={(body) => (bodyRef.current = body)}
             />
           </View>
+          {file && (
+            <View style={styles.file}>
+              {getFileType(file) == "video" ? (
+                <Video
+                  source={{ uri: getFileUri(file) }}
+                  resizeMode="cover"
+                  useNativeControls
+                  style={{ flex: 1 }}
+                  isLooping
+                />
+              ) : (
+                <Image
+                  source={{ uri: getFileUri(file) }}
+                  resizeMode="cover"
+                  style={{ flex: 1 }}
+                />
+              )}
+              <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
+                <Icon name="delete" size={20} color="white" />
+              </Pressable>
+            </View>
+          )}
+          <View style={styles.media}>
+            <Text style={styles.addImageText}>Add to your post</Text>
+            <View style={styles.mediaIcons}>
+              <TouchableOpacity onPress={() => onPick(true)}>
+                <Icon name="image" size={30} color={theme.colors.dark} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => onPick(false)}>
+                <Icon name="video" size={33} color={theme.colors.dark} />
+              </TouchableOpacity>
+            </View>
+          </View>
         </ScrollView>
+        <Button
+          buttonStyle={{ height: hp(6.5) }}
+          title="Post"
+          loading={loading}
+          hasShadow={false}
+          onPress={onSubmit}
+        />
       </View>
     </ScreenWrapper>
   );
@@ -90,8 +216,16 @@ const styles = StyleSheet.create({
     borderCurve: "continuous",
     borderColor: theme.colors.gray,
   },
-  mediaIcons: {},
-  addImageText: {},
+  mediaIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  addImageText: {
+    fontSize: hp(1.9),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
   imageIcon: { borderRadius: theme.radius.md },
   file: {
     height: hp(30),
@@ -105,5 +239,8 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 10,
     right: 10,
+    backgroundColor: "rgba(255,0,0,0.6)",
+    padding: 7,
+    borderRadius: 50,
   },
 });
--- components/RichTextEditor.jsx
+++ components/RichTextEditor.jsx
@@ -1,42 +1,79 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { RichToolbar, actions } from "react-native-pell-rich-editor";
+import {
+  RichToolbar,
+  actions,
+  RichEditor,
+} from "react-native-pell-rich-editor";
+import { theme } from "../constants/theme";
 
 const RichTextEditor = ({ editorRef, onChange }) => {
   return (
     <View style={{ minHeight: 285 }}>
-      <Text>
         <RichToolbar
           actions={[
-            actions.insertImage,
+            actions.setStrikethrough,
+            actions.removeFormat,
             actions.setBold,
             actions.setItalic,
-            actions.insertBulletsList,
             actions.insertOrderedList,
-            actions.insertLink,
-            actions.keyboard,
-            actions.setStrikethrough,
-            actions.setUnderline,
-            actions.removeFormat,
-            actions.insertVideo,
-            actions.checkboxList,
-            actions.undo,
-            actions.redo,
+            actions.blockquote,
+            actions.alignLeft,
+            actions.alignCenter,
+            actions.alignRight,
+            actions.code,
+            actions.line,
+            actions.heading1,
+            actions.heading4,
           ]}
+          iconMap={{
+            [actions.heading1]:({tintColor}) => <Text style={{color:tintColor}}>H1</Text>,
+            [actions.heading4]:({tintColor}) => <Text style={{color:tintColor}}>H4</Text>
+          }}
           style={styles.richBar}
-          flatContainerStyle={styles.listStyle}
+          selectedIconTint={theme.colors.primaryDark}
+          flatContainerStyle={styles.flatStyle}
           editor={editorRef}
           disabled={false}
-
         />
-      </Text>
-    </View>
+        <RichEditor
+          disabled={false}
+          ref={editorRef}
+          containerStyle={styles.rich}
+          editorStyle={styles.contentStyle}
+          placeholder={"what's on your mind?"}
+          style={{width: '100%'}}
+
+          onChange={onChange}
+        />
+      </View>
   );
 };
 
 export default RichTextEditor;
 
 const styles = StyleSheet.create({
-  richBar:{},
-  listStyle:{},
+  richBar: {
+    borderTopRightRadius: theme.radius.xl,
+    borderTopLeftRadius: theme.radius.xl,
+    backgroundColor: theme.colors.gray,
+  },
+  rich: {
+    minHeight: 240,
+    flex: 1,
+    borderWidth: 1.5,
+    borderTopWidth: 0,
+    borderBottomLeftRadius: theme.radius.xl,
+    borderBottomRightRadius: theme.radius.xl,
+    borderColor: theme.colors.gray,
+    padding: 5,
+  },
+  contentStyle: {
+    color: theme.colors.textDark,
+    placeholderColor: "grey",
+  },
+  flatStyle: {
+    paddingHorizontal: 8,
+    gap:3
+  },
 });
